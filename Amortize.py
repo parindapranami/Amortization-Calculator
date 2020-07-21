e1dcbdfed0f3e748347ecc943a6bbee2361ba317
Parinda Pranami <parindapranami@gmail.com>
2020-07-21 18:41:28 +0530

    Added assert

--- Amortize.py
+++ Amortize.py
@@ -9,6 +9,9 @@ parser.add_argument('-P','--P',type = float,help = 'Principal amount')
 parser.add_argument('-r','--r',type = float,help = 'rate of interest per period') 
 parser.add_argument('-n','--n',type = int,help = 'Tenure') 
 args = parser.parse_args()
+assert args.P > 0,"Principal should be a positive non zero number"
+assert args.r > 0,"Rate should be a positive non zero number"
+assert args.n > 0,"Tenure should be a positive non zero number"
 
 if __name__ == "__main__":
   loan = Loan(args.P,args.r/1200,args.n*12) 
--- loan.py
+++ loan.py
@@ -1,32 +1,3 @@
-# Parinda, regarding converting things to object oriented. Here are some hints.
-# If I write a program that uses this Loan class, I want to be able to do the following. 
-# 1) Create a Loan object like this.
- 
-#     loan = Loan(150000, 4.5, 360)
-
-# 2) I should be able to use properties on the loan like this.
-
-# print(loan.principal)
-# print(loan.interest)
-# print(loan.numberOfPeriods)
-
-# 3) I should also be able to print payment like this.
-
-# print(loan.paymentPerPeriod)
-
-# 4) I should be able to get payment schedule (array of dictionaries) like this 
-
-# schedule = loan.generateAmortizationSchedule()
-
-# 5) I should be able to print the schedule like this.
-
-# loan.printAmortizationSchedule()
-
-# 6) All the code should be in a "Loan.py" file
-#  The properties above are named the way I would name in java or swift (lowerCamelCase). If python has different convention for naming properties, then you should find out and use that. If you can't figure out, use lowerCamelCase style.
-#  After you implement Loan.py, it should be very easy to change command line program to use Loan.py instead.
-
-
 class Loan:
 
   def __init__(self,P,r,n):
@@ -34,6 +5,7 @@ class Loan:
     self.rate = r 
     self.numberOfPeriods = n 
     self.payment = self.principal*(self.rate*((1 + self.rate)**self.numberOfPeriods) / (((1 + self.rate)**self.numberOfPeriods) - 1))
+    assert self.payment > 0,"Monthly Amortization payment cannot be negative"
     self.schedule = self.amortization_schedule()
     
   def amortization_schedule(self):
@@ -44,13 +16,17 @@ class Loan:
     total_interest = 0
     while period < self.numberOfPeriods:
       monthly_interest = balance * self.rate
+      assert monthly_interest > 0,"Interest should be a positive number"
       amount =  self.payment - monthly_interest
+      assert amount > 0,"Amount should be a positive number"
       balance = balance - amount
+      #assert balance > 0,"Balance should be a positive number"
       period+=1
       total_interest += monthly_interest
       schedule_dict = dict(Period=period,Interest=monthly_interest,Principal=amount,Balance=balance)
       schedule.append(schedule_dict) 
     self.total_interest = total_interest
+    assert self.total_interest > 0,"""Total interest can't be negative"""
     return schedule
 
   def print_schedule(self):
